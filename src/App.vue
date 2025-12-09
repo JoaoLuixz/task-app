18af057b8afedfcda3c11d24e590331ac05105a1
João Luiz <joaoluizmartins6@gmail.com>
2025-12-09 11:05:04 -0300

    feat: move task list to it own component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import type { Task } from './types'
 import TaskForm from './components/TaskForm.vue'
+import TaskList from './components/TaskList.vue'
 
 const tasks = ref<Task[]>([])
 
@@ -9,15 +10,17 @@ function addTask(newTaskContent: string) {
   const newTask: Task = { id: crypto.randomUUID(), content: newTaskContent, isDone: false }
   tasks.value.push(newTask)
 }
+
+function toggleTask(taskId: string) {
+  const task = tasks.value.find((task) => task.id === taskId)
+  if (task) task.isDone = !task.isDone
+}
 </script>
 
 <template>
   <h1>Task App</h1>
   <TaskForm @addTask="addTask" />
-  <div v-for="task in tasks" :key="task.id">
-    <p>{{ task.content }}</p>
-    <input type="checkbox" :checked="task.isDone" />
-  </div>
+  <TaskList :tasks @toggleTask="toggleTask" />
 </template>
 
 <style scoped></style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Task } from '@/types'
+
+defineProps<{ tasks: Task[] }>()
+const emit = defineEmits<{ toggleTask: [taskId: string] }>()
+
+function toggleTask(taskId: string) {
+  emit('toggleTask', taskId)
+}
+</script>
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <p>{{ task.content }}</p>
+    <input type="checkbox" @click="toggleTask(task.id)" :checked="task.isDone" />
+  </div>
+</template>
