f842002f53d0803ed3c041a3c6460c6c6e2ac68b
João Luiz <joaoluizmartins6@gmail.com>
2025-12-09 10:46:31 -0300

    refactor: move task form to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Task } from './types'
+import TaskForm from './components/TaskForm.vue'
 
 const tasks = ref<Task[]>([])
-const newTaskContent = ref<string>('')
-function addTask() {
-  if (!newTaskContent.value.trim()) return
-  const newTask: Task = { id: crypto.randomUUID(), content: newTaskContent.value, isDone: false }
+
+function addTask(newTaskContent: string) {
+  const newTask: Task = { id: crypto.randomUUID(), content: newTaskContent, isDone: false }
   tasks.value.push(newTask)
-  newTaskContent.value = ''
 }
 </script>
 
 <template>
   <h1>Task App</h1>
-
-  <form @submit.prevent="addTask">
-    <input v-model="newTaskContent" />
-    <button>Add</button>
-  </form>
-
+  <TaskForm @addTask="addTask" />
   <div v-for="task in tasks" :key="task.id">
     <p>{{ task.content }}</p>
     <input type="checkbox" :checked="task.isDone" />
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{ addTask: [taskContent: string] }>()
+const newTaskContent = ref<string>('')
+
+function addTask() {
+  if (!newTaskContent.value.trim()) return
+  emit('addTask', newTaskContent.value)
+  newTaskContent.value = ''
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTask">
+    <input v-model="newTaskContent" />
+    <button>Add</button>
+  </form>
+</template>
