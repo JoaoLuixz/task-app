ab219d8987874196bb344f3d8ccfe777bccc22d9
João Luiz <joaoluizmartins6@gmail.com>
2026-07-15 11:17:56 -0300

    fix: add missing Storage update method

--- src/composables/storage/storage.ts
+++ src/composables/storage/storage.ts
@@ -4,5 +4,6 @@ export interface Storage<T> {
   store(data: T): Promise<Partial<{ error: Error; data: T }>>;
   get(id: number): Promise<Partial<{ error: Error; data: T }>>;
   getAll(): Promise<Partial<{ error: Error; data: T[] }>>;
+  update(id: number): Promise<Partial<{ error: Error; data: T }>>;
   remove(id: number): Promise<Partial<{ error: Error; data: T }>>;
 }
